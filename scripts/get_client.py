9e6bad3fc06a1d6e653a03fbb494aaa1d5c708c4
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2022-09-26 10:49:12 +0000

    Use relative path for FreeIPA config

--- scripts/get_client.py
+++ scripts/get_client.py
@@ -1,9 +1,12 @@
 from python_freeipa import ClientMeta
 import yaml
+import os
 
 
 def get_client() -> ClientMeta:
-    with open("freeipa_config.yml", "r") as stream:
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(script_dir,'freeipa_config.yml')
+    with open(config_path, "r") as stream:
         freeipa_config = yaml.safe_load(stream)
     hostname = freeipa_config["hostname"]
     username = freeipa_config["username"]
@@ -11,4 +14,3 @@ def get_client() -> ClientMeta:
     client = ClientMeta(hostname, verify_ssl=False)
     client.login(username=username, password=password)
     return client
-
